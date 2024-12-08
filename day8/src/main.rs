6e12950e8c9a81736d15c4c26e8f042c50e7db53
Linus Dikomey <l.dikomey03@gmail.com>
2024-12-08 15:42:21 +0100

    day 8 refactored

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -19,47 +19,44 @@ fn main() {
     }
     let in_bounds =
         |v: Vec2i| (0..input[0].len() as _).contains(&v.x) && (0..input.len() as _).contains(&v.y);
-    let part1 = antennas
-        .values()
-        .flat_map(|antennas| {
-            antennas.iter().flat_map(|&a1| {
-                antennas
-                    .iter()
-                    .copied()
-                    .filter(move |&a2| a1 != a2)
-                    .flat_map(move |a2| [a1 - (a2 - a1), a2 + (a2 - a1)])
-            })
-        })
-        .collect::<HashSet<Vec2i>>()
-        .into_iter()
-        .filter(|&l| in_bounds(l))
-        .count();
+
+    let part1 = antinode_count(&antennas, in_bounds, move |a1, a2| {
+        [a1 - (a2 - a1), a2 + (a2 - a1)]
+    });
     println!("part 1: {part1}");
-    let part2 = antennas
-        .values()
-        .flat_map(|antennas| {
-            antennas.iter().flat_map(|&a1| {
-                antennas
-                    .iter()
-                    .copied()
-                    .filter(move |&a2| a1 != a2)
-                    .flat_map(move |a2| {
-                        let step = a2 - a1;
-                        let mut p = a1;
-                        while in_bounds(p - step) {
-                            p -= step;
-                        }
-                        iterate(p, move |&p| p + step).take_while(move |&p| in_bounds(p))
-                    })
-            })
-        })
-        .collect::<HashSet<Vec2i>>()
-        .into_iter()
-        .filter(|&l| in_bounds(l))
-        .count();
+    let part2 = antinode_count(&antennas, in_bounds, move |a1, a2| {
+        let step = a2 - a1;
+        let mut p = a1;
+        while in_bounds(p - step) {
+            p -= step;
+        }
+        iterate(p, move |&p| p + step).take_while(move |&p| in_bounds(p))
+    });
     println!("part 2: {part2}");
 }
 
 fn is_antenna(x: u8) -> bool {
     x.is_ascii_alphanumeric()
+}
+
+fn antinode_count<F: Copy + Fn(Vec2i, Vec2i) -> O, O: IntoIterator<Item = Vec2i>>(
+    antennas: &HashMap<u8, Vec<Vec2i>>,
+    in_bounds: impl Copy + Fn(Vec2i) -> bool,
+    interferences: F,
+) -> usize {
+    antennas
+        .values()
+        .flat_map(|antennas| {
+            antennas.iter().flat_map(|&a1| {
+                antennas
+                    .iter()
+                    .copied()
+                    .filter(move |&a2| a1 != a2)
+                    .flat_map(move |a2| interferences(a1, a2))
+            })
+        })
+        .collect::<HashSet<Vec2i>>()
+        .into_iter()
+        .filter(|&l| in_bounds(l))
+        .count()
 }
