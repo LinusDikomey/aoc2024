1cdeb76be0e03eae123be0538886016ffd0ac308
LinusDikomey <l.dikomey03@gmail.com>
2024-12-11 02:10:25 +0100

    refactor day10 with aoch grid

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,34 +1,27 @@
 use std::collections::HashSet;
 
+use aoch::{Grid, Vec2i};
+
 fn main() {
-    let input: Vec<Vec<u8>> = include_str!("../../input/day10.txt")
-        .trim()
-        .lines()
-        .map(|line| line.as_bytes().iter().map(|&b| b - b'0').collect())
-        .collect();
+    let g = Grid::from_str_bytes(include_str!("../../input/day10.txt").trim()).map(|b| b - b'0');
     println!("Hello, world!");
-    let part1: usize = input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, &h)| (x, y, h)))
-        .filter(|&(_, _, h)| h == 0)
-        .map(|(x, y, h)| {
+    let part1: usize = g
+        .positions()
+        .filter(|&p| g[p] == 0)
+        .map(|p| {
             let mut s = HashSet::new();
-            traverse(&input, x, y, h, &mut |p| {
+            traverse(&g, p, 0, &mut |p| {
                 s.insert(p);
             });
             s.len()
         })
         .sum();
     println!("Part 1: {part1}");
-    let part2: usize = input
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, &h)| (x, y, h)))
-        .filter(|&(_, _, h)| h == 0)
-        .map(|(x, y, h)| {
+    let part2: usize = g
+        .positions()
+        .map(|p| {
             let mut count = 0;
-            traverse(&input, x, y, h, &mut |_| {
+            traverse(&g, p, 0, &mut |_| {
                 count += 1;
             });
             count
@@ -37,23 +30,14 @@ fn main() {
     println!("Part 2: {part2}");
 }
 
-fn traverse<F: FnMut((usize, usize))>(map: &[Vec<u8>], x: usize, y: usize, h: u8, found: &mut F) {
-    if map[y][x] != h {
+fn traverse<F: FnMut(Vec2i)>(g: &Grid<u8>, p: Vec2i, h: u8, found: &mut F) {
+    if g[p] != h {
         return;
     } else if h == 9 {
-        found((x, y));
+        found(p);
         return;
     }
-    if x > 0 {
-        traverse(map, x - 1, y, h + 1, found);
-    }
-    if y > 0 {
-        traverse(map, x, y - 1, h + 1, found);
-    }
-    if x < map[0].len() - 1 {
-        traverse(map, x + 1, y, h + 1, found);
-    }
-    if y < map.len() - 1 {
-        traverse(map, x, y + 1, h + 1, found);
+    for p in g.neighbor_positions4(p) {
+        traverse(g, p, h + 1, found);
     }
 }
